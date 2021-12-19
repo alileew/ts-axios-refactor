ccbbcae61a32f63bc436e96ec5ab8ea5dba1742b
alileew <alileew.me@gmail.com>
2021-12-19 17:49:32 +0800

    feat: add url common logic

---
+++ examples/base/app.ts
@@ -0,0 +1,38 @@
+import axios from '../../src/index'
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: ['bar', 'baz']
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: {
+      bar: 'baz'
+    }
+  }
+})
+
+const date = new Date()
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    date
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: '@:$，'
+  }
+})
+
