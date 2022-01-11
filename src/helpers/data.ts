3d592e5315a5efe865aa648cd8eb191227eb0115
alileew <alileew.me@gmail.com>
2022-01-11 22:35:31 +0800

    feat: add other axios config

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -1,4 +1,5 @@
 import { isPlainObject } from './util'
+
 export function transformRequest(data: any): any {
   if (isPlainObject(data)) {
     return JSON.stringify(data)
@@ -10,8 +11,8 @@ export function transformResponse(data: any): any {
   if (typeof data === 'string') {
     try {
       data = JSON.parse(data)
-    } catch (error) {
-      console.log(error)
+    } catch (e) {
+      // do nothing
     }
   }
   return data
--- src/helpers/headers.ts
+++ src/helpers/headers.ts
@@ -1,10 +1,10 @@
-import { isPlainObject } from './util'
+import { isPlainObject, deepMerge } from './util'
+import { Method } from '../types'
 
 function normalizeHeaderName(headers: any, normalizedName: string): void {
   if (!headers) {
     return
   }
-
   Object.keys(headers).forEach(name => {
     if (name !== normalizedName && name.toUpperCase() === normalizedName.toUpperCase()) {
       headers[normalizedName] = headers[name]
@@ -15,6 +15,7 @@ function normalizeHeaderName(headers: any, normalizedName: string): void {
 
 export function processHeaders(headers: any, data: any): any {
   normalizeHeaderName(headers, 'Content-Type')
+
   if (isPlainObject(data)) {
     if (headers && !headers['Content-Type']) {
       headers['Content-Type'] = 'application/json;charset=utf-8'
@@ -24,23 +25,35 @@ export function processHeaders(headers: any, data: any): any {
 }
 
 export function parseHeaders(headers: string): any {
-  console.log('headers', headers)
-
   let parsed = Object.create(null)
   if (!headers) {
     return parsed
   }
 
   headers.split('\r\n').forEach(line => {
-    let [key, val] = line.split(':')
+    let [key, ...vals] = line.split(':')
     key = key.trim().toLowerCase()
     if (!key) {
       return
     }
-    if (val) {
-      val = val.trim()
-    }
+    const val = vals.join(':').trim()
     parsed[key] = val
   })
+
   return parsed
+}
+
+export function flattenHeaders(headers: any, method: Method): any {
+  if (!headers) {
+    return headers
+  }
+  headers = deepMerge(headers.common, headers[method], headers)
+
+  const methodsToDelete = ['delete', 'get', 'head', 'options', 'post', 'put', 'patch', 'common']
+
+  methodsToDelete.forEach(method => {
+    delete headers[method]
+  })
+
+  return headers
 }
